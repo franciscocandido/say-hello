5f3a71c14f9cbc2fe5c7fea3c349a3c3057bd2a2
Francisco Cãndido <franciscocandidoal@gmail.com>
2020-08-22 00:18:35 -0300

    🚧 Login page in progress

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,11 +1,79 @@
 import React from 'react';
+import * as yup from 'yup';
+
+import { Formik, Form, Field } from 'formik';
+
+import { Container } from './styles';
 
 import DefaultPage from '../../Components/DefaultPage';
+import Input from '../../Components/Input';
+import Button from '../../Components/Button';
+import Select from '../../Components/Select';
+import Languages from '../../Data/languages.json';
 
 function Login() {
+  // Language Selector
+  const arraySize = Languages.languages.length;
+  const languagesOptions = [];
+  for (var c = 0; c < arraySize; c += 1) {
+    languagesOptions
+      .push(
+        <option
+          key={Languages.languages[c].code}
+          value={Languages.languages[c].code}
+        >
+          {Languages.languages[c].name}
+        </option>,
+      );
+  }
+
+  const initialValues = {
+    username: '',
+  };
+
+  const validate = yup.object().shape({
+    username: yup.string()
+      .max(15, 'Fifteen characters or less')
+      .required('This field is required'),
+  });
+
   return (
     <DefaultPage>
-      <h1>Login Page</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validate}
+      >
+        {({
+          values, handleChange, handleBlur, handleSubmit,
+        }) => (
+          <Container>
+            <Form onSubmit={handleSubmit}>
+              <Field
+                as={Input}
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="usernameInput"
+                name="username"
+                type="text"
+                label="Username"
+                placeholder="Your username"
+              />
+              <Select
+                id="languageSelect"
+                label="Select Language (Optional)"
+                defaultValue=""
+              >
+                <option disabled value="">Select a Language</option>
+                {languagesOptions}
+              </Select>
+              <Button
+                value="Login"
+              />
+            </Form>
+          </Container>
+        )}
+      </Formik>
     </DefaultPage>
   );
 }
---
+++ src/Pages/Login/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 84vh;
+  button {
+    margin-top: 20px;
+  }
+`;
