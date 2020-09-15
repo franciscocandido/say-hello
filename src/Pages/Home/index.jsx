c8945d93e0975482d58d96c950aa23bc0dbd1a15
FranciscoCandido <franciscocandidoal@gmail.com>
2020-09-15 17:29:01 -0300

    ✨ "Hello" page finished

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import DefaultPage from '../../Components/DefaultPage';
 
+import { Container, HelloText, UsernameText } from './styles';
+
 function Home() {
+  const userData = {
+    username: localStorage.getItem('@sayhello/username'),
+    language: localStorage.getItem('@sayhello/language'),
+  };
+  const [helloMessage, setHelloMessage] = useState('Say Hello');
+  const renderHTML = (rawHTML) => React.createElement('div', { dangerouslySetInnerHTML: { __html: rawHTML } });
+
+  async function getHelloMessageByIp() {
+    const userIpData = await axios.get('http://ip-api.com/json')
+      .then((response) => response);
+    const userIp = userIpData.data.query;
+
+    const languageCodeData = await axios.get(`https://fourtonfish.com/hellosalut/?ip=${userIp}`)
+      .then((response) => response);
+    const languageCode = languageCodeData.data.hello;
+
+    return languageCode;
+  }
+
+  async function getHelloMessageByCode(code) {
+    const languageCodeData = await axios.get(`https://fourtonfish.com/hellosalut/?lang=${code}`)
+      .then((response) => response);
+    const languageCode = languageCodeData.data.hello;
+
+    return languageCode;
+  }
+
+  async function makeHelloMessage() {
+    let message;
+    if (!userData.username) {
+      userData.username = 'User';
+    }
+
+    if (!userData.language) {
+      message = await getHelloMessageByIp();
+    } else {
+      message = await getHelloMessageByCode(userData.language);
+    }
+    setHelloMessage(message);
+  }
+  makeHelloMessage();
+
   return (
     <DefaultPage>
-      <h1>Home</h1>
+      <Container>
+        <HelloText>{renderHTML(`${helloMessage}`)}</HelloText>
+        <UsernameText>{userData.username}</UsernameText>
+      </Container>
     </DefaultPage>
   );
 }
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.h1`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+`;
+
+export const HelloText = styled.h1`
+  margin: 0;
+  margin-top: 15%;
+`;
+
+export const UsernameText = styled.h1`
+  margin: 0;
+  color: var(--support);
+`;
